aa0483b4be3502cf27b054b2fd6ea33285b73d01
Dhaker Achour <achour.dhaker@gmail.com>
2023-05-22 08:46:51 -0700

    Task 0 FIX

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-'''
-Python script that returns information using REST API
-'''
+"""
+Shows the complete tasks done by a specific employee ID
+"""
+
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(
-                    url, user)).json()
-            alltsk = len(jreq)
-            completedtsk = []
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+    # Get the todo list
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    params = (('userId', sys.argv[1]),)
+    t = requests.get(url, params=params)
+
+    # Get the user info
+    url = 'https://jsonplaceholder.typicode.com/users'
+    params = (('id', sys.argv[1]),)
+    u = requests.get(url, params=params)
+
+    # Convert to json
+    todos = t.json()
+    user = u.json()
+
+    done = []
+    for task in todos:
+        if task['completed'] is True:
+            done.append(task)
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(user[0]['name'], len(done), len(todos)))
+    if len(done) > 0:
+        for task in done:
+            print("\t {}".format(task['title']))
