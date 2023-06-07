61971db90b662b0147363ea0dab0ba42b3501eef
Dhaker Achour <achour.dhaker@gmail.com>
2023-06-07 10:47:02 -0700

    FIX

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python3
-""" recursive function that queries the Reddit API"""
+"""Find the number of subs"""
 import requests
-import sys
-after = None
-count_dic = []
+import shlex
+
+def print_words(word_list=[], hot_list=[]):
+    print(word_list)
+    print(len(hot_list))
+    for word in word_list:
+        count = 0
+        print(word)
+        for title in top_post:
+            if word in shlex.split(title):
+                count += 1
+        print("{}: {}".format(word, count))
 
 
-def count_words(subreddit, word_list):
-    """parses the title of all hot articles, and prints a sorted count of given
-    keywords (case-insensitive, delimited by spaces) """
-    global after
-    global count_dic
-    headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
+
+def count_words(subreddit, word_list=[], hot_list=[], after=""):
+    """ show the top 10 posts in a subreddit """
+    try:
+        # Change the user agent
+        headers = {'User-Agent': 'cmmolanos'}
+        payload = {'t': 'all', 'after': after}
+        request = requests.get('https://api.reddit.com/r/{}/hot'.
+                               format(subreddit), headers=headers,
+                               params=payload)
+        top_posts = request.json()
+
+        for post in top_posts['data']['children']:
+            hot_list.append(post['data']['title'])
+
+        after = top_posts['data']['after']
+        print(word_list)
+        print(after)
+        if after is not None:
+            count_words(subreddit, word_list, hot_list, after)
+
+        return (print_words(word_list, hot_list))
+
+    except:
+        return None
