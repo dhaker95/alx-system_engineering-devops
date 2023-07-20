91aeda5a64a76ff461a487a2ce0b05747af8d528
Dhaker Achour <103954430+dhaker95@users.noreply.github.com>
2023-07-20 23:11:01 +0100

    Update 0-the_sky_is_tge_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_tge_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_tge_limit_not.pp
@@ -1,7 +1,14 @@
+# Increse the request's limit
 
-ur stack so that we get to 0 errors
-exec { 'file limit':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+# Increse ulimit value
+exec { 'fix-config-nginx':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
+
+# Restart nginex service
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
