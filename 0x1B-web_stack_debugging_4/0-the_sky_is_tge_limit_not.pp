d4251aa266f142cae81c695656ac9d4b14a9355c
Dhaker Achour <103954430+dhaker95@users.noreply.github.com>
2023-07-20 23:44:11 +0100

    Update 0-the_sky_is_tge_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_tge_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_tge_limit_not.pp
@@ -1,6 +1,3 @@
-# fix our stack so that there is not failed requests
-
-exec { 'change nginx limit':
-    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
-    provider => shell,
-}
+# Puppet script
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
