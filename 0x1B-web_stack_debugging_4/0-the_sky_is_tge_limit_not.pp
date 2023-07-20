f68cb4dbfcd5b2f75d805146c8b5a37cda307f26
Dhaker Achour <103954430+dhaker95@users.noreply.github.com>
2023-07-20 23:53:18 +0100

    Update 0-the_sky_is_tge_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_tge_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_tge_limit_not.pp
@@ -1,7 +1,7 @@
 #0-the_sky_is_the_limit_not.pp set open file limit higher
 exec { 'set limit to 2000':
   path    => '/bin',
-  command => "sed -i 's/15/2000/' /etc/default/nginx"
+  command => "sed -i 's/100/2000/' /etc/default/nginx"
 }
 exec { 'reboot nginx':
   command => '/usr/sbin/service nginx restart'
